3cfe9a54ab5c7f50c15e084ec494bb41c53ccecd
Kassy0220 <shinbokutaro0220@gmail.com>
2024-06-20 08:43:46 +0900

    ルートページを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "minutes#index"
 
   namespace :api do
     resources :minutes, only: [:show, :update] do
