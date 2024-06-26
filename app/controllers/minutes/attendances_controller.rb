091c511da12e4e53a3cb974c815b3fef5ea04017
Kassy0220 <shinbokutaro0220@gmail.com>
2024-06-26 20:47:45 +0900

    タイポを修正

--- app/controllers/minutes/attendances_controller.rb
+++ app/controllers/minutes/attendances_controller.rb
@@ -1,5 +1,5 @@
 class Minutes::AttendancesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:new, :edit, :create, :update]
+  skip_before_action :authenticate_member!, only: [:new, :edit, :create, :update]
   before_action :set_attendance, only: %i[ edit update destroy ]
 
   # GET /attendances/new
