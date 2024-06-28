e6d4a0f3b506c260c00d196c7a3dc5bcf0600065
Kassy0220 <shinbokutaro0220@gmail.com>
2024-06-28 13:52:26 +0900

    議事録モデルの関連付けを修正

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -1,5 +1,5 @@
 class Minute < ApplicationRecord
-  has_many :topics
+  has_many :topics, dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :members, through: :attendances
 end
