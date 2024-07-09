560951021b9e390cdf75c055ffa319207dbf62aa
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-09 19:54:10 +0900

    議事録のMarkdownで、データ取得と表示の部分を切り離した

--- app/models/minute_template.rb
+++ app/models/minute_template.rb
@@ -1,8 +1,8 @@
 class MinuteTemplate
   DEFAULT_PATH = 'config/minute_template/minute.md'
 
-  def self.build(minute, day_attendees, night_attendees, topics, absent_members)
+  def self.build(minute, day_attendees, night_attendees, topics, formatted_date,  absent_members)
     template = File.read(DEFAULT_PATH)
-    ERB.new(template).result_with_hash({ minute:, day_attendees:, night_attendees:, topics:, absent_members: })
+    ERB.new(template).result_with_hash({ minute:, day_attendees:, night_attendees:, topics:, next_date: formatted_date, absent_members: })
   end
 end
