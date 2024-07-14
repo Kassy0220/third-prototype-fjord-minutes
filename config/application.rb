e6b7a055e947902c7fd52e854a58bb5015f00c50
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-14 13:17:28 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module ThirdPrototypeFjordMinutes
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Tokyo"
   end
 end
