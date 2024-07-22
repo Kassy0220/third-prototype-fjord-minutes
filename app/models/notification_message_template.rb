5b1198a51d38434b4579a83177a26df10a542693
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-22 10:26:52 +0900

    NotificationMessageTemplateをリファクタリング

--- app/models/notification_message_template.rb
+++ app/models/notification_message_template.rb
@@ -1,13 +1,22 @@
 class NotificationMessageTemplate
-  extend DateHelper
+  include DateHelper
   ROLE_ID = ENV['TEAM_MEMBER_ROLE_ID'].to_i
+  MESSAGE_TEMPLATE_FOR_MINUTE_CREATION = 'config/message_template/minute_creation.md'
+
   def self.create_message(message_type, course, minute)
-    template = case message_type
-               when :create
-                 File.read('config/message_template/minute_creation.md')
-               else
-                 File.read('config/message_template/minute_creation.md')
-               end
-    ERB.new(template).result_with_hash({ role_id: ROLE_ID, course_name: course.name, meeting_date: format_date(minute.date), url: "https://fjord_minutes/minutes/#{minute.id}/attendances/new" })
+    new(message_type).create_message(course, minute)
+  end
+
+  def initialize(message_type)
+    @template = case message_type
+                when :create
+                  File.read(MESSAGE_TEMPLATE_FOR_MINUTE_CREATION)
+                else
+                  File.read(MESSAGE_TEMPLATE_FOR_MINUTE_CREATION)
+                end
+  end
+
+  def create_message(course, minute)
+    ERB.new(@template).result_with_hash({ role_id: ROLE_ID, course_name: course.name, meeting_date: format_date(minute.date), url: "https://fjord_minutes/minutes/#{minute.id}/attendances/new" })
   end
 end
