af6e9d51ac43a88bb824c28a2c1e85a87ad5e7b3
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-22 10:04:25 +0900

    日付の成形を行うヘルパーメソッドを独自に切り出した

--- app/helpers/minutes_helper.rb
+++ app/helpers/minutes_helper.rb
@@ -1,2 +1,18 @@
 module MinutesHelper
+  def attendees_list(members)
+    members.map{ |member_email| "- [#{member_name(member_email)}](#)" }.join("\n    ")
+  end
+
+  def absentees_list(members)
+    members.map{ |member| "- [#{member_name(member[0])}](#)\n  - 進捗報告: #{member[1]}" }.join("\n")
+  end
+
+  def member_name(email)
+    email.slice(/^[^@]+/)
+  end
+
+  def topics_list(topics)
+    topics.map{ |topic| "- #{topic.content}" }.join("\n")
+  end
 end
+
