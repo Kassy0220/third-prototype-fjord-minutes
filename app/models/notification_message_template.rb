b162182b4fa48ca8c06577d265411ff585c54e6f
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-22 18:45:43 +0900

    通知メッセージの参加登録URLを、仮のURLから正しいURLに変更

--- app/models/notification_message_template.rb
+++ app/models/notification_message_template.rb
@@ -1,4 +1,5 @@
 class NotificationMessageTemplate
+  include Rails.application.routes.url_helpers
   include DateHelper
   ROLE_ID = ENV['TEAM_MEMBER_ROLE_ID'].to_i
   MESSAGE_TEMPLATE_FOR_MINUTE_CREATION = 'config/message_template/minute_creation.md'
@@ -17,6 +18,6 @@ class NotificationMessageTemplate
   end
 
   def create_message(course, minute)
-    ERB.new(@template).result_with_hash({ role_id: ROLE_ID, course_name: course.name, meeting_date: format_date(minute.date), url: "https://fjord_minutes/minutes/#{minute.id}/attendances/new" })
+    ERB.new(@template).result_with_hash({ role_id: ROLE_ID, course_name: course.name, meeting_date: format_date(minute.date), url: new_minute_attendance_url(minute) })
   end
 end
