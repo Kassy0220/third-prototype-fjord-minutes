f0696b66cfa1bb6b6675c68e8065886632695d8f
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-24 16:15:48 +0900

    Minute#meeting_was_already_held?のテストを追加

--- test/models/minute_test.rb
+++ test/models/minute_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class MinuteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#meeting_was_already_held?" do
+    course = Course.find_by(name: "Railsエンジニアコース")
+    minute = course.minutes.create(date: Time.zone.parse('2024-07-03'), next_date: Time.zone.parse('2024-07-17'))
+
+    travel_to Time.zone.parse('2024-07-02') do
+      refute minute.meeting_was_already_held?
+    end
+
+    travel_to Time.zone.parse('2024-07-03') do
+      refute minute.meeting_was_already_held?
+    end
+
+    travel_to Time.zone.parse('2024-07-04') do
+      assert minute.meeting_was_already_held?
+    end
+  end
 end
