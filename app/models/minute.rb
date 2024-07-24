5f3eeb008203b5791d07a2a469bd20e22496231b
Kassy0220 <shinbokutaro0220@gmail.com>
2024-07-24 16:17:51 +0900

    議事録が過去のものであるかを判別するために、議事録モデルにメソッドを追加

--- app/models/minute.rb
+++ app/models/minute.rb
@@ -3,4 +3,8 @@ class Minute < ApplicationRecord
   has_many :topics, dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :members, through: :attendances
+
+  def meeting_was_already_held?
+    date.before? Time.zone.today
+  end
 end
