d2aef5b3e48afc57a4d184f91e2d7dadf056df5f
Kassy0220 <shinbokutaro0220@gmail.com>
2024-08-07 10:07:37 +0900

    議事録の内容から次回開催日を取得する際、正規表現でマッチさせるのに不要な空白を削除

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,6 +1,6 @@
 module DateHelper
   def format_date(date)
     day_of_the_week = %w(日 月 火 水 木 金 土)[date.wday]
-    "#{date.strftime('%Y年%m月%d日')} (#{day_of_the_week})"
+    "#{date.strftime('%Y年%m月%d日')}(#{day_of_the_week})"
   end
 end
