b096081b4a074d0ad5be414e1804e544d9e44c58
Kassy0220 <shinbokutaro0220@gmail.com>
2024-08-12 19:32:41 +0900

    Memberモデルに、GitHub認証用のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_12_102754) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_103133) do
   create_table "attendances", force: :cascade do |t|
     t.integer "time"
     t.string "absence_reason"
@@ -40,6 +40,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_12_102754) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "avatar_url"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
