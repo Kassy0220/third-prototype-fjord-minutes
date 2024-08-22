ce1cfaf3dcf32194f5a5179e971f01c8c05f7dfd
Kassy0220 <shinbokutaro0220@gmail.com>
2024-08-23 06:54:28 +0900

    開発・テスト環境用に、簡易的なサインアップを利用できるようにした

--- app/controllers/members/omniauth_callbacks_controller.rb
+++ app/controllers/members/omniauth_callbacks_controller.rb
@@ -1,23 +1,33 @@
 class Members::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Devise::Controllers::Rememberable
-  skip_before_action :verify_authenticity_token, only: :github
+  skip_before_action :verify_authenticity_token, only: [:github, :developer]
   skip_before_action :prohibit_hiatus_member_access, only: :github
 
   def github
+    authenticate_with_omniauth('GitHub')
+  end
+
+  def developer
+    authenticate_with_omniauth('Developer')
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def authenticate_with_omniauth(provider)
     @member = Member.from_omniauth(request.env["omniauth.auth"], request.env["omniauth.params"])
 
     if @member.persisted?
       @member.hiatuses.last.update(finished_at: Time.zone.now) if @member.hiatus?
       sign_in_and_redirect @member
       remember_me @member
-      set_flash_message(:notice, :success, kind: "GitHub") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
     else
       session["devise.github_data"] = request.env["omniauth.auth"].except(:extra)
       redirect_to new_member_registration_url
     end
-  end
-
-  def failure
-    redirect_to root_path
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,12 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['AUTH_APP_ID'], ENV['AUTH_APP_SECRET']
+  if Rails.env.production?
+    config.omniauth :github, ENV['AUTH_APP_ID'], ENV['AUTH_APP_SECRET']
+  else
+    config.omniauth :github, ENV['AUTH_APP_ID'], ENV['AUTH_APP_SECRET']
+    config.omniauth :developer, fields: [:email, :nickname, :image], uid_field: :email
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
